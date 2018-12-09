6559c18913bcc7ad9c510819c42d80a99469e588
guojingming <1043840980@qq.com>
2018-12-09 16:54:35 +0800

    has some bugs

--- scripts/camera.py
+++ scripts/camera.py
@@ -10,7 +10,7 @@ while(1):
     # get a frame
     ret, frame = cap.read()
     # show a frame
-    cv2.imshow("capture", frame)
+    #cv2.imshow("capture", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
