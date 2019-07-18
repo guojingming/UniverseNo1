a3ec866fffac122ab61c096970265829acbaa31d
guojingming <1043840980@qq.com>
2019-07-18 18:38:49 +0800

    latest

--- scripts/universe_one.py
+++ scripts/universe_one.py
@@ -1,3 +1,14 @@
-import motor_driver as md
+import cv2
 
-md.test()
+from yolo_utils import yolo_util as yu
+
+
+param = yu.init_params("../")
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img_ori = cap.read()
+    img = yu.get_predict_result(img_ori, param)
+    cv2.imshow("res", img)
+    cv2.waitKey(1)
