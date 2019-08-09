c224ae7e346b3a87a3c453b7184f55a8005e086e
guojingming <1043840980@qq.com>
2019-08-09 10:49:31 +0800

    'latest'

--- local/scripts/universe_one.py
+++ local/scripts/universe_one.py
@@ -3,21 +3,21 @@ import config as cfg
 
 import threading
 import time
-'''
+
 detection_server = DetectionServer(cfg.server_address, cfg.server_port)
 detection_server.start()
 while True:
     print(detection_server.get_tags())
-'''
-
-def process():
-    while True:
-        print("%s" % threading.current_thread().name)
 
 
-if __name__ == '__main__':
-    for i in range(10):
-        t = threading.Thread(target=process)
-        t.start()
-    while True:
-        print("aaaaaaaaaaaaaaaaaaaaaaaa")
+# def process():
+#     while True:
+#         print("%s" % threading.current_thread().name)
+#
+#
+# if __name__ == '__main__':
+#     for i in range(10):
+#         t = threading.Thread(target=process)
+#         t.start()
+#     while True:
+#         print("aaaaaaaaaaaaaaaaaaaaaa")
